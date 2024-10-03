0249a28d08cb839c121334339a742d3b07a1e1b1
Saldana <lmsaldana@cps.k12.il.us>
2024-10-03 09:32:47 -0500

    https://github.com/Mangoyummn/week6/blob/main/sets_tuples.py

--- sets_tuples.py
+++ sets_tuples.py
@@ -1,125 +1,123 @@
 
 
-# Sets##################################
-# Sets are unordered collections of unique elements
-# Sets are mutable
-# Sets are defined by curly braces {}
-#example of sets
-set1 = {1, 2, 3, 4, 5}  # set of integers
-set2 = {'apple', 'banana', 'cherry'}  # set of strings
-set3 = {1, 2, 3, 'apple', 'banana'}  # mixed set
-set4 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5}  # duplicate elements are removed
+# # Sets##################################
+# # Sets are unordered collections of unique elements
+# # Sets are mutable
+# # Sets are defined by curly braces {}
+# #example of sets
+# set1 = {1, 2, 3, 4, 5}  # set of integers
+# set2 = {'apple', 'banana', 'cherry'}  # set of strings
+# set3 = {1, 2, 3, 'apple', 'banana'}  # mixed set
+# set4 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5}  # duplicate elements are removed
 
 
-#access elements in a set
-print(set1)#Output:{1,2.3,4,5)
-print(1 in set1) # Output:true 
-print(6 in set1) # Output :False
-print("apple" in set2) #Output :True
-print("banana"in set3) #Output:True
-# You cannot acess elements in a set by index because sets are unordered 
-# instead you convert the set to a list amd acess elements by index
-list=list(set1)
-#We use sets when we want to store unique elements
-#We use sets when we dont care about the order of the elements
-#and do not want the elements to be changed
+# #access elements in a set
+# print(set1)#Output:{1,2.3,4,5)
+# print(1 in set1) # Output:true 
+# print(6 in set1) # Output :False
+# print("apple" in set2) #Output :True
+# print("banana"in set3) #Output:True
+# # You cannot acess elements in a set by index because sets are unordered 
+# # instead you convert the set to a list amd acess elements by index
+# list=list(set1)
+# #We use sets when we want to store unique elements
+# #We use sets when we dont care about the order of the elements
+# #and do not want the elements to be changed
 
-# add elements to a set
-print(set1.add(6)) #Output:{1.2.3.4.5.)
-print(set2.add('orange')) # Output :{'apple', 'banana', 'cherry','orange'} 
-print(set3.add('strawberry'))
-print(set4.add (15)) 
-#remove elements from a set
-print(set1.remove (6))
-print(set2.remove('orange')) #Output :{'apple', 'banana', 'cherry'} 
-print(set3.remove('strawberry'))
-#check if an element is in a set
-print(1 in set1) # Output:True 
+# # add elements to a set
+# print(set1.add(6)) #Output:{1.2.3.4.5.)
+# print(set2.add('orange')) # Output :{'apple', 'banana', 'cherry','orange'} 
+# print(set3.add('strawberry'))
+# print(set4.add (15)) 
+# #remove elements from a set
+# print(set1.remove (6))
+# print(set2.remove('orange')) #Output :{'apple', 'banana', 'cherry'} 
+# print(set3.remove('strawberry'))
+# #check if an element is in a set
+# print(1 in set1) # Output:True 
 
-#find the length of a set
-print(len (set1)) # Output:5
+# #find the length of a set
+# print(len (set1)) # Output:5
 
-#clear a set
-print(set1.clear()) # Output : set ()
-
-
-
-#tuples##################################
-# Tuples are ordered collections of elements
-# Tuples are immutable
-# Tuples are defined by parentheses ()
-#example of tuples
-tuple1 = (1, 2, 3, 4, 5)  # tuple of integers
-tuple2 = ('apple', 'banana', 'cherry')  # tuple of strings
-tuple3 = (1, 2, 3, 'apple', 'banana')  # mixed tuple
-tuple4 = (1, 2, 3, 4, 5, 1, 2, 3, 4, 5)  # duplicate elements are allowed
-
-
-#access elements in a tuple
-print(tuple1)#Output :()
-print(tuple1[0]) #Output:1
-print(tuple2[1])# Output:'banna'
-# print(tuple3{3}) # Output:'apple'
-#find the length of a tuple
-print(len(tuple1)) # Output:5
-print(len(tuple2)) # Output:3
-#count the number of occurrences of an element in a tuple
-print(tuple4.count(1)) # Output: 2
-print(tuple4.count(5))# Output:0
-#find the index of an element in a tuple
-print(tuple2.index('banana'))#Output:1
-print(tuple2.index ('cherry'))
-
-#convert a tuple to a list
-print(list(tuple1)) # Output:[1,2,3,4,5]
-
-#convert a list to a tuple
-print(tuple(tuple1)) #Output"(1,2,3,4,5)
+# #clear a set
+# print(set1.clear()) # Output : set ()
 
 
 
+# #tuples##################################
+# # Tuples are ordered collections of elements
+# # Tuples are immutable
+# # Tuples are defined by parentheses ()
+# #example of tuples
+# tuple1 = (1, 2, 3, 4, 5)  # tuple of integers
+# tuple2 = ('apple', 'banana', 'cherry')  # tuple of strings
+# tuple3 = (1, 2, 3, 'apple', 'banana')  # mixed tuple
+# tuple4 = (1, 2, 3, 4, 5, 1, 2, 3, 4, 5)  # duplicate elements are allowed
 
 
+# #access elements in a tuple
+# print(tuple1)#Output :()
+# print(tuple1[0]) #Output:1
+# print(tuple2[1])# Output:'banna'
+# # print(tuple3{3}) # Output:'apple'
+# #find the length of a tuple
+# print(len(tuple1)) # Output:5
+# print(len(tuple2)) # Output:3
+# #count the number of occurrences of an element in a tuple
+# print(tuple4.count(1)) # Output: 2
+# print(tuple4.count(5))# Output:0
+# #find the index of an element in a tuple
+# print(tuple2.index('banana'))#Output:1
+# print(tuple2.index ('cherry'))
 
+# #convert a tuple to a list
+# print(list(tuple1)) # Output:[1,2,3,4,5]
 
-#######################tuples challenge#####################
-# Challenge: Count the number of occurrences of the character 'v' in the text below.
-# The text is converted to a tuple of characters and the target characters are 'v' and 'V'.
-# The result is output to the console.
-#queue the videos(2)
-text = """Voilà! In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate.
-This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished. However, this valorous visitation
-of a by-gone vexation stands vivified, and has vowed to vanquish these venal and virulent vermin vanguarding vice and
-vouchsafing the violently vicious and voracious violation of volition.
-
-
-The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day
-vindicate the vigilant and the virtuous.
-
-
-Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it is my very good honor to meet you
-and you may call me V."""
-
-
-# Convert the text to a tuple of characters
-text_tuple= tuple(text)
-print(len(text_tuple))
-#lowercase_v=text_tuplelower()
-#print(lowercase_v)
-
-# Tuple to store the target characters
-
-
-small_v= text_tuple.count('v')
-capital_v=text_tuple.count('V')
-
-
-# Count occurrences of 'v' or 'V' by filtering the text_tuple
+# #convert a list to a tuple
+# list1=(1,2,3,4,5)
+# print(tuple(list1)) #Output:(1,2,3,4,5)
 
 
 
 
-# Output the result
+
+
+# #######################tuples challenge#####################
+# # Challenge: Count the number of occurrences of the character 'v' in the text below.
+# # The text is converted to a tuple of characters and the target characters are 'v' and 'V'.
+# # The result is output to the console.
+# #queue the videos(2)
+# text = """Voilà! In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate.
+# This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished. However, this valorous visitation
+# of a by-gone vexation stands vivified, and has vowed to vanquish these venal and virulent vermin vanguarding vice and
+# vouchsafing the violently vicious and voracious violation of volition.
+
+
+# The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day
+# vindicate the vigilant and the virtuous.
+
+
+# Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it is my very good honor to meet you
+# and you may call me V."""
+
+
+# # Convert the text to a tuple of characters
+# text_tuple= tuple(text)
+# print(len(text_tuple))
+
+# # Tuple to store the target characters
+
+
+# small_v= text_tuple.count('v')
+# capital_v=text_tuple.count('V')
+
+
+# # Count occurrences of 'v' or 'V' by filtering the text_tuple
+
+# total=small_v + capital_v
+
+
+# # Output the result
 
 
 
@@ -134,43 +132,53 @@ sample_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(len(sample_list)) # Output: 3
 #this is called a nested list
 # Extract and print the second element from the first list
-
+print(sample_list[0][1]) #Output:2
+print(sample_list[1][2])#Output:6
+print(sample_list[2][0])#Output:7
+print(sample_list[2][2])#Output:9
+print(sample_list[0][0])#Output:1
 
 sample_list_of_fruit = {"fruits": ["apple", "banana", "cherry"]}
 # Extract and print the second fruit from the list
-
+print(sample_list_of_fruit["fruits"][1]) #Output:'banana'
+print(sample_list_of_fruit['fruits'][2]) #Output:'cherry'
 
 sample_list_of_lists = {"lists": [[1, 2, 3], [4, 5, 6], [7, 8, 9]]}
-# Extract and print the third element from the second list
+# # Extract and print the third element from the second list
+# print(sample_list_of_lists["lists"][1][2]) #Output:6
+# print(sample_list_of_lists["lists"][2][1]) #Output:8
+# print(sample_list_of_lists["lists"][0][2]) #Output:3
+# print(sample_list_of_lists["lists"][2][0])#Output:7
+
+# sample_list_of_dicts = {"dicts": [{"name": "Alice", "age": 25}, {"name": "Bob", "age": 30}, {"name": "Charlie", "age": 35}]}
+# # Extract and print the age of the second person
+
+# print(sample_list_of_dicts["dicts"][1]["age"]) #Output:30
+# print(sample_list_of_dicts["dicts"][2]["name"]) #Output:charalie
 
 
 
-
-sample_list_of_dicts = {"dicts": [{"name": "Alice", "age": 25}, {"name": "Bob", "age": 30}, {"name": "Charlie", "age": 35}]}
-# Extract and print the age of the second person
-
-
-
+# data = {
+#     "fruits": {"tropical": ["mango", "pineapple", "banana"], "berries": ["strawberry", "blueberry", "raspberry"]},
+#     "prices": {"mango": 1.5, "pineapple": 2.5, "banana": 0.5}
+# }
 
 
+# # Extract and print the second item from the 'tropical' list
+# print(data["fruits"]["tropical"][1])  # Output: 'pineapple'
 
-data = {
-    "fruits": {"tropical": ["mango", "pineapple", "banana"], "berries": ["strawberry", "blueberry", "raspberry"]},
-    "prices": {"mango": 1.5, "pineapple": 2.5, "banana": 0.5}
-}
-
-
-# Extract and print the second item from the 'tropical' list
-print(data["fruits"]["tropical"][1])  # Output: 'pineapple'
-
-
-
+# print(data["fruits"]["berries"][2])#Output:rasberry
+# print(data["prices"]["mango"]) #Output:1.5
+# print(data["prices"]["banana"]) # Output:0.5
 
 # Define the dictionary
 info = {
     "team": {"coach": {"name": "John Doe", "age": 45}, "players": ["Alice", "Bob", "Charlie"]},
-    "location": "New York"
-}
+    "location": "New York"}
+
+print(info["team"]["coach"]['age'])#Ouput:45
+print(info["team"]["players"][2])
+print(info["location"]) #Ouput:new york
 
 
 # Extract and print the coach's name
@@ -194,9 +202,11 @@ company = {
 }
 
 
+
 # Extract and print the second employee from the 'Engineering' department
 print(company["departments"]["Engineering"]["employees"][1])  # Output: 'Noah'
-
+print(company["departments"]["Engineering"]["budget"]) #Output:120000
+print(company["department"]["Hr"]["budget"]+["Engineering"]["budget"]) 
 
 # Define the dictionary
 school = {
